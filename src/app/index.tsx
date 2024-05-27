5587f9171032ba10c639b6ba91cb084bbb74371e
Ivan Bitkov <iambitkov@gmail.com>
2024-05-28 03:47:27 +0700

    Made a layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,16 @@
 import "./styles/index.scss"
 import { MainPage } from "pages/MainPage"
+import { LogoSVG } from "shared/svg/logo"
 
-const Index = () => {
-  return <MainPage />
+const App = () => {
+  return (
+    <div className="wrapper">
+      <MainPage />
+      <div className="logo">
+        <LogoSVG />
+      </div>
+    </div>
+  )
 }
 
-export default Index
+export default App
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,8 +2,37 @@ import styles from "./MainPage.module.scss"
 
 export const MainPage = () => {
   return (
-    <div className={styles.container}>
-      <h1>Hello, world?</h1>
+    <div className={styles.pageContainer}>
+      <video
+        src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        controls
+        className={styles.videoPlayer}
+      />
+      <div className={styles.eventsContainer}>
+        <h2>Список событий:</h2>
+        <div className={styles.eventsButtons}>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+          <button>23:03:23</button>
+        </div>
+      </div>
     </div>
   )
 }
