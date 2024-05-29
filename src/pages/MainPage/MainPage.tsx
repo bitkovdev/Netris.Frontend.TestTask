1800d1befe5aa0d2101d6f26f02c16232c5a713f
Ivan Bitkov <iambitkov@gmail.com>
2024-05-29 14:41:41 +0700

    Switching between timecodes has been made, as well as video broadcast inside the canvas.

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,25 +1,96 @@
 import styles from "./MainPage.module.scss"
-import { useEffect } from "react"
+import React, { useEffect, useRef } from "react"
 import { fetchGetEventsList } from "shared/api"
 import { useUnit } from "effector-react"
 import { $EventsList } from "entities/EventsModel/model"
 import { VIDEO_ROOT } from "shared/config/env"
 
 export const MainPage = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
   const eventsList = useUnit($EventsList)
 
   useEffect(() => {
     fetchGetEventsList({})
   }, [])
 
+  useEffect(() => {
+    if (canvasRef.current && videoRef.current) {
+      const canvas = canvasRef.current
+      const video = videoRef.current
+      const ctx = canvas.getContext("2d")
+
+      // Setting canvas size
+      video.addEventListener("loadedmetadata", function () {
+        canvas.width = video.videoWidth
+        canvas.height = video.videoHeight
+      })
+
+      // Click handler for pause video
+      const pauseVideo = () => {
+        if (!video.paused) {
+          video.pause()
+          canvas.removeEventListener("click", pauseVideo)
+        }
+      }
+
+      // Click handler for play video
+      const playVideo = async () => {
+        if (video.paused) {
+          await video.play()
+          canvas.removeEventListener("click", playVideo)
+          canvas.addEventListener("click", pauseVideo)
+        }
+      }
+
+      canvas.addEventListener("click", playVideo)
+
+      if (ctx) {
+        video.addEventListener("play", function () {
+          // I couldn’t think of another way to live broadcast from a video tag. So I had to ignore eslint at this point.
+          // eslint-disable-next-line @typescript-eslint/no-this-alias
+          const $this = this // cache
+          ;(function loop() {
+            if (!$this.paused && !$this.ended) {
+              ctx.drawImage($this, 0, 0)
+              setTimeout(loop, 1000 / 24) // drawing at 24fps
+            }
+          })()
+        })
+      }
+    }
+  }, [canvasRef, videoRef])
+
+  const setTimestamp = async (timestamp: number) => {
+    if (canvasRef.current && videoRef.current) {
+      const canvas = canvasRef.current
+      const video = videoRef.current
+      const ctx = canvas.getContext("2d")
+
+      if (ctx) {
+        video.currentTime = timestamp
+        // If the video is paused, the handler below will change the current freeze frame
+        if (video.paused) {
+          video.ontimeupdate = () => ctx.drawImage(video, 0, 0)
+        }
+      }
+    }
+  }
+
   return (
     <div className={styles.pageContainer}>
-      <video src={VIDEO_ROOT} controls className={styles.videoPlayer} />
+      <canvas ref={canvasRef} className={styles.videoPlayer} />
+      <video ref={videoRef} src={VIDEO_ROOT} muted hidden />
       <div className={styles.eventsContainer}>
         <h2>Список событий:</h2>
         <div className={styles.eventsButtons}>
           {eventsList.map(item => (
-            <button key={item.timestamp}>{item.time_code}</button>
+            <button
+              onClick={() => setTimestamp(item.timestamp)}
+              key={item.timestamp}
+            >
+              {item.time_code}
+            </button>
           ))}
         </div>
       </div>
